62841abf000118214d4275510475e9139fb866d9
ag0st1nh0 <ma.agostinho9@gmail.com>
2024-08-15 11:41:33 -0300

    Tudo salvo

---
+++ empresarios/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('cadastrar_empresa/', views.cadastrar_empresa, name="cadastrar_empresa"),
+    path('listar_empresas/', views.listar_empresas, name="listar_empresas"),
+    path('empresa/<int:id>', views.empresa, name="empresa"),
+    path('add_doc/<int:id>', views.add_doc, name="add_doc"),
+    path('excluir_dc/<int:id>', views.excluir_dc, name="excluir_dc"),
+    path('add_metrica/<int:id>', views.add_metrica, name="add_metrica"),
+    path('empresas/', views.listar_empresas, name='empresas'),
+    path('gerenciar_proposta/<int:id>', views.gerenciar_proposta, name="gerenciar_proposta")
+]
---
+++ investidores/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import PropostaInvestimento
+
+admin.site.register(PropostaInvestimento)
